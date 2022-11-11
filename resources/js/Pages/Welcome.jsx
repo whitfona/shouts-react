11ac7932ee18fa2904efdd6b756bfeec36637aaf
Nick <nicholas@wealthyplanet.com>
2022-11-11 12:18:55 -0500

    Style guest layout

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -85,69 +85,91 @@ export default function Welcome(props) {
     return (
         <>
             <Head title="Welcome" />
-            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-                <div className="fixed top-0 right-0 px-6 py-4 sm:block">
-                    {props.auth.user ? (
-                        <Link href={route('dashboard')} className="text-sm text-gray-700 dark:text-gray-500 underline">
-                            Dashboard
-                        </Link>
-                    ) : (
-                        <>
-                            <Link href={route('login')} className="text-sm text-gray-700 dark:text-gray-500 underline">
-                                Log in
-                            </Link>
+            <div className="relative flex items-top justify-center min-h-screen bg-pink-400 dark:bg-gray-900 sm:items-center sm:pt-0">
+                {/*<div className="fixed top-0 right-0 px-6 py-4 sm:block">*/}
+                {/*    {props.auth.user ? (*/}
+                {/*        <Link href={route('dashboard')} className="text-sm text-gray-700 dark:text-gray-500 underline">*/}
+                {/*            Dashboard*/}
+                {/*        </Link>*/}
+                {/*    ) : (*/}
+                {/*        <>*/}
+                {/*            <Link href={route('login')} className="text-sm text-white dark:text-gray-500 underline">*/}
+                {/*                Log in*/}
+                {/*            </Link>*/}
 
-                            <Link
-                                href={route('register')}
-                                className="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"
-                            >
-                                Register
-                            </Link>
-                        </>
-                    )}
-                </div>
+                {/*            <Link*/}
+                {/*                href={route('register')}*/}
+                {/*                className="ml-4 text-sm text-white dark:text-gray-500 underline"*/}
+                {/*            >*/}
+                {/*                Register*/}
+                {/*            </Link>*/}
+                {/*        </>*/}
+                {/*    )}*/}
+                {/*</div>*/}
 
                 <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                    <div className="flex flex-col pt-8 sm:justify-start sm:pt-0">
-                        <ApplicationLogo size={50} />
-                        <h1>SHOUTS!</h1>
+                    <div className="flex justify-between pt-8">
+                        <div className="flex flex-col justify-center items-center">
+                            <ApplicationLogo size={50} />
+                            <h1 className="text-white">SHOUTS!</h1>
+                        </div>
+                        <div>
+                            {props.auth.user ? (
+                                <Link href={route('dashboard')} className="text-sm text-gray-700 dark:text-gray-500 underline">
+                                    Dashboard
+                                </Link>
+                            ) : (
+                                <>
+                                    <Link href={route('login')} className="text-sm text-white dark:text-gray-500 underline">
+                                        Log in
+                                    </Link>
+
+                                    <Link
+                                        href={route('register')}
+                                        className="ml-4 text-sm text-white dark:text-gray-500 underline"
+                                    >
+                                        Register
+                                    </Link>
+                                </>
+                            )}
+                        </div>
                     </div>
 
                     <div className="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                            <div className="py-12">
-                                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                                        <div className="p-6 bg-white border-b border-gray-200">Welcome</div>
-                                        <button onClick={startReader} className="p-4 bg-red-400">Scan Beer</button>
-                                        <p>{message}</p>
-                                        <div id="reader"></div>
-                                        <div>
-                                            {beers.map(beer => (
-                                                <div key={beer.id}>
-                                                    <p>Barcode: {beer.barcode}</p>
-                                                    <p>Name: {beer.name}</p>
-                                                    <p>Brewery: {beer.brewery}</p>
-                                                    <p>Percent: {beer.alcohol_percent}</p>
-                                                    <p>Type: {beer.category.type}</p>
-                                                    {beer.rating.map(rating => (
-                                                        <ol key={rating.id}>
-                                                            <li>User: {rating.user.name}</li>
-                                                            <li>Rating: {rating.rating}</li>
-                                                            <li>Comment: {rating.comment}</li>
-                                                        </ol>
-                                                    ))}
-                                                    <br /><br />
-                                                </div>
-                                            ))}
-                                        </div>
+                        <div className="py-12">
+                            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                                <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                                    <div className="p-6 bg-white border-b border-gray-200">Welcome</div>
+                                    <button onClick={startReader} className="p-4 bg-red-400">Scan Beer</button>
+                                    <p>{message}</p>
+                                    <div id="reader"></div>
+                                    <div>
+                                        {beers.map(beer => (
+                                            <div key={beer.id}>
+                                                <p>Barcode: {beer.barcode}</p>
+                                                <p>Name: {beer.name}</p>
+                                                <p>Brewery: {beer.brewery}</p>
+                                                <p>Percent: {beer.alcohol_percent}</p>
+                                                <p>Type: {beer.category.type}</p>
+                                                {beer.rating.map(rating => (
+                                                    <ol key={rating.id}>
+                                                        <li>User: {rating.user.name}</li>
+                                                        <li>Rating: {rating.rating}</li>
+                                                        <li>Comment: {rating.comment}</li>
+                                                    </ol>
+                                                ))}
+                                                <br /><br />
+                                            </div>
+                                        ))}
                                     </div>
                                 </div>
                             </div>
+                        </div>
                     </div>
 
                     <div className="flex justify-center mt-4 sm:items-center sm:items-center">
                         <div className="text-center text-sm text-gray-500 sm:text-left">
-                                <a href="https://whitforddesign.ca">whitforddesign.ca</a>
+                            <a href="https://whitforddesign.ca">whitforddesign.ca</a>
                         </div>
                     </div>
                 </div>
