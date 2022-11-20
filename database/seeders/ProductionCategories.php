ea3681a7e1ea555003e4c5b958d05ffb15c74935
Nick <nicholas@wealthyplanet.com>
2022-11-20 08:02:07 -0500

    Make production categories seeder

---
+++ database/seeders/ProductionCategories.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class ProductionCategories extends Seeder
+{
+    /**
+     * Run the database category seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::factory()->create(['type' => 'Sour']);
+        Category::factory()->create(['type' => 'Ale']);
+        Category::factory()->create(['type' => 'Cider']);
+        Category::factory()->create(['type' => 'Lager']);
+        Category::factory()->create(['type' => 'Porter']);
+        Category::factory()->create(['type' => 'Radler']);
+        Category::factory()->create(['type' => 'Stout']);
+        Category::factory()->create(['type' => 'Wheat']);
+    }
+}
