22312cde36783d980fb85f6913717458cc032f1e
Nick <nicholas@wealthyplanet.com>
2022-11-20 15:14:15 -0500

    Create AllBeers and BeersByBarcode controllers

---
+++ app/Http/Controllers/Private/GET/AllBeersController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Private\GET;
+
+use App\Http\Resources\BeerResource;
+use App\Models\Beer;
+use Illuminate\Routing\Controller;
+
+class AllBeersController extends Controller
+{
+    /**
+     * GET all beers with all users ratings & comments for each beer
+     *
+     */
+    public function __invoke()
+    {
+        $allBeers = BeerResource::collection(Beer::all());
+
+        return response()->json($allBeers);
+    }
+}
