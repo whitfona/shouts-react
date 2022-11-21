b063cda5245625acc6138ba3a0fbd498b483acb7
Nick <nicholas@wealthyplanet.com>
2022-11-21 17:34:55 -0500

    Change front end to use full file path

--- resources/js/Components/PublicBeerDetails.jsx
+++ resources/js/Components/PublicBeerDetails.jsx
@@ -4,8 +4,7 @@ import DetailFormat from "@/Components/DetailFormat";
 export default function PublicBeerDetails({beer, searchByBrewery, searchByUser}) {
     return (
         <div className="border-t-8 border-pink-100 bg-white md:flex gap-4 p-4" key={beer.id}>
-            <img className="w-[192px] mb-3 md:mb-0" src={`http://localhost:8000/storage/beers/${beer.photo}`} />
-            {/*<img className="w-[192px] mb-3 md:mb-0" src={`https://itsyourshout.ca/storage/beers/${beer.photo}`} />*/}
+            <img className="w-[192px] mb-3 md:mb-0" src={beer.photo} />
             <div className="w-full">
                 <div className="md:flex flex-wrap justify-between gap-x-2 gap-y-0 mb-3">
                     <DetailFormat name={'Name'} value={beer.name} />
@@ -30,8 +29,7 @@ export default function PublicBeerDetails({beer, searchByBrewery, searchByUser})
                                 <p>
                                     <span className="text-md font-semibold tracking-wide uppercase">Added By: </span>
                                     <button onClick={() => searchByUser(rating.user_id)}>
-                                        {rating.user_photo && <img className="w-8 h-8 rounded-full inline" src={`http://localhost:8000/storage/users/${rating.user_photo}`} />} {rating.user}</button> | {rating.date_added}</p>
-                                        {/*{rating.user_photo && <img className="w-8 h-8 rounded-full inline" src={`https://itsyourshout.ca/storage/users/${rating.user_photo}`} />} {rating.user}</button> | {rating.date_added}</p>*/}
+                                        {rating.user_photo && <img className="w-8 h-8 rounded-full inline" src={rating.user_photo} />} {rating.user}</button> | {rating.date_added}</p>
                                 <p><span className="text-md font-semibold tracking-wide uppercase">Rating: </span>{rating.rating}</p>
                                 <p><span className="text-md font-semibold tracking-wide uppercase">Comment: </span>{rating.comment}</p>
                             </div>
