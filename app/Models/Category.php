8bfe790e0f76a498b1c25e494b518087c5ae8ce4
Nick <nicholas@wealthyplanet.com>
2022-11-21 19:34:07 -0500

    Add public routes to api.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,6 +10,8 @@ class Category extends Model
 {
     use HasFactory;
 
+    protected $hidden = ['created_at', 'updated_at'];
+
     public function beer(): HasMany
     {
         return $this->HasMany(Beer::class);
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\Public\Get\AllBeersController;
+use App\Http\Controllers\Public\Get\AllCategoriesController;
+use App\Http\Controllers\Public\Get\BeersByBarcodeController;
+use App\Http\Controllers\Public\Get\BeersByBreweryController;
+use App\Http\Controllers\Public\Get\BeersByCategoryController;
+use App\Http\Controllers\Public\Get\BeersBySearchController;
+use App\Http\Controllers\Public\Get\BeersByUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +20,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::prefix('beers')->group(function () {
+    Route::get('/', AllBeersController::class)->name('beers.index');
+    Route::get('/barcode/{beer}', BeersByBarcodeController::class)->name('beers.barcode.show');
+    Route::get('/brewery/{beer}', BeersByBreweryController::class)->name('beers.brewery.show');
+    Route::get('/category/{beer}', BeersByCategoryController::class)->name('beers.category.show');
+    Route::get('/all/user/{beer}', BeersByUserController::class)->name('beers.user.show');
+    Route::get('/search/{beer}', BeersBySearchController::class)->name('beers.search.show');
+});
+Route::get('/categories', AllCategoriesController::class)->name('categories.index');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
